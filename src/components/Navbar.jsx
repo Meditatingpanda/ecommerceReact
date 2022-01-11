9fde2a102607ffe165ccb2d4d189bae31482c6d9
Traveling Monk <grp.gyanaranajan@gmail.com>
2022-01-12 00:12:53 +0530

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import "../style/main.css";
+import { Search, ShoppingCart } from "@material-ui/icons";
+
+const Navbar = () => {
+  return (
+    <div className="  bg-blue-600 h-24  sm:h-20  flex items-center justify-center">
+      <div className=" flex  items-center sm:justify-around  w-[95%] h-7">
+        <div className="sm:w-[60%] grid grid-cols-2 sm:flex w-full">
+          <div>
+            <img
+              src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fk-plus_3b0baa.png"
+              className=" ml-3 mb-3 h-6 sm:h-8"
+            />
+          </div>
+          <div className="  justify-self-end mr-1 cursor-pointer sm:hidden">
+            <i class="fas fa-bars text-white text-lg"></i>
+          </div>
+
+          <div className="sm:w-[50%]  col-start-1 col-span-2 ml-2">
+            <div className="flex ">
+              <input
+                type="text"
+                className=" rounded rounded-r-none pl-3 pt-2 pb-2 w-full  text-sm focus:outline-none"
+                placeholder="search here"
+              />
+
+              <div className="bg-white  rounded-r flex items-center">
+                <Search className="text-blue-600 cursor-pointer" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="text-base sm:flex  text-white  hidden sm:visible ">
+          <div className="mr-5">Login</div>
+          <div className="mr-5">Sign Up</div>
+          <div className="mr-5">
+            <ShoppingCart />
+            Cart
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
