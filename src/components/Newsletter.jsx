025bdfba7cec0e87b9d92a4f5498419b59a36dbf
Traveling Monk <grp.gyanaranajan@gmail.com>
2022-01-13 03:13:15 +0530

    make responisve

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,18 +2,18 @@ import { Telegram } from "@material-ui/icons";
 
 const Newletter = () => {
   return (
-    <div className="bg-[#FCF5F5] h-[45vh] mt-10 flex flex-col items-center justify-center">
-      <h1 className="text-6xl mb-4">Newsletter</h1>
-      <p className="text-base mb-3">
+    <div className="bg-[#FCF5F5] h-[30vh] sm:h-[45vh] mt-10 flex flex-col items-center justify-center">
+      <h1 className="text-xl sm:text-6xl mb-4">Newsletter</h1>
+      <p className="text-xs sm:text-base mb-3">
         Get timely updates from your favorite products.
       </p>
-      <div className="flex">
+      <div className="flex w-full  justify-center">
         <input
           type="email"
-          className="focus:outline-none p-2 pl-3 text-sm w-[40rem]"
+          className="focus:outline-none p-2 pl-3 text-sm w-[50%]"
           placeholder="Your Email"
         />
-        <div className="bg-[#008080] w-[5rem] flex items-center justify-center text-white">
+        <div className="bg-[#008080] w-[8%] flex items-center justify-center text-white">
           <Telegram />
         </div>
       </div>
